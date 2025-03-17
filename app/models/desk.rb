2c2b0e4efb396de5eddad5b01feea12a5664b215
yuekami <yuekami7@gmail.com>
2025-03-17 13:08:55 +0900

    デザイン修正

--- app/models/desk.rb
+++ app/models/desk.rb
@@ -3,4 +3,8 @@ class Desk < ApplicationRecord
   validates :content, length: { maximum: 65_535 }
   mount_uploader :desk_image, DeskImageUploader
   belongs_to :user
+
+  def neo_url
+    self.desk_image.url.gsub('upload/', 'upload/w_600/')
+  end
 end
